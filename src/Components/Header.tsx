b315cba1a0599538857ac6ec2dc4862c6934ce27
Rohit-vunet <rohitkg@vunetsystems.com>
2025-02-10 15:42:12 +0530

    Notes_app

---
+++ src/Components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Toolbar,AppBar} from '@mui/material'
+
+const Header = () => {
+  return (
+  
+        <AppBar>
+              <Toolbar>
+                
+              </Toolbar>
+
+        </AppBar>
+   
+   
+  )
+}
+
+export default Header
